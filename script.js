4c5fc650ce405a2944bc7e7179a7363537e73144
mohamed <mohamedell@gmai.com>
2026-03-24 14:38:10 +0100

    convert the rock paper scissors game to an UI interface

--- script.js
+++ script.js
@@ -2,79 +2,100 @@ const human_choise = document.getElementById("human-choise");
 const computer_choise = document.getElementById("computer-choise");
 const result = document.getElementById("result");
 const score = document.getElementById("score");
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissors = document.getElementById("scissors");
 
-let choises = ["rock", "paper", "scissors"];
 let humanScore = 0;
 let computerScore = 0;
 
-function getComputerChoice(items) {
-    return (computer_choise.innerHTML = `${items[Math.floor(Math.random() * items.length)]}`);
+function getComputerChoice() {
+    let choises = ["rock", "paper", "scissors"];
+    return (computer_choise.textContent = `${choises[Math.floor(Math.random() * choises.length)]}`);
 }
+let start = document.createElement("button");
+start.id = "start";
+start.textContent = "Start Game";
+let reset = document.createElement("button");
+reset.textContent = "Reset";
+let rock = document.createElement("button");
+rock.id = "rock";
+rock.textContent = "Rock";
+let paper = document.createElement("button");
+paper.id = "paper";
+paper.textContent = "Paper";
+let scissors = document.createElement("button");
+scissors.id = "scissors";
+scissors.textContent = "Scissors";
 
 function getHumanChoice() {
-    let rock = document.createElement("button");
-    rock.id = "rock";
-    rock.innerHTML = "Rock";
-    let paper = document.createElement("button");
-    paper.id = "paper";
-    paper.innerHTML = "Paper";
-    let scissors = document.createElement("button");
-    scissors.id = "scissors";
-    scissors.innerHTML = "Scissors";
+    human_choise.append(rock, paper, scissors, reset, start);
 
-    human_choise.appendChild(rock);
-    human_choise.appendChild(paper);
-    human_choise.appendChild(scissors);
+    rock.style.display = "none";
+    paper.style.display = "none";
+    scissors.style.display = "none";
+    reset.style.display = "none";
+
     rock.addEventListener("click", function handlRock() {
-        playRound(rock.id, getComputerChoice(choises));
+        playRound(rock.id, getComputerChoice());
     });
     paper.addEventListener("click", function handlPaper() {
-        playRound(paper.id, getComputerChoice(choises));
+        playRound(paper.id, getComputerChoice());
     });
     scissors.addEventListener("click", function handlScissors() {
-        playRound(scissors.id, getComputerChoice(choises));
+        playRound(scissors.id, getComputerChoice());
+    });
+    reset.addEventListener("click", function () {
+        humanScore = 0;
+        computerScore = 0;
+        result.textContent = "";
+        score.textContent = "";
+        computer_choise.textContent = "";
+    });
+    start.addEventListener("click", function () {
+        start.style.display = "none";
+
+        rock.style.display = "inline";
+        paper.style.display = "inline";
+        scissors.style.display = "inline";
+        reset.style.display = "inline";
     });
 }
-function stopGame() {
-    rock.removeEventListener("click", handlRock);
-    paper.removeEventListener("click", handlPaper);
-    scissors.removeEventListener("click", handlScissors);
-    return;
-}
+
 function playRound(humanChoise, computerChoise) {
     if (
         (humanChoise === "paper" && computerChoise === "rock") ||
         (humanChoise === "rock" && computerChoise === "scissors") ||
         (humanChoise === "scissors" && computerChoise === "paper")
     ) {
-        result.innerHTML = "You Win 🏆";
+        result.textContent = "You Win 🏆";
         humanScore++;
-        score.innerHTML = `${humanScore} : ${computerScore}`;
+        score.textContent = `${humanScore} : ${computerScore}`;
     }
     if (
         (humanChoise === "rock" && computerChoise === "paper") ||
         (humanChoise === "scissors" && computerChoise === "rock") ||
         (humanChoise === "paper" && computerChoise === "scissors")
     ) {
-        result.innerHTML = "You Lose ❌";
+        result.textContent = "You Lose ❌";
         computerScore++;
-        score.innerHTML = `${humanScore} : ${computerScore}`;
+        score.textContent = `${humanScore} : ${computerScore}`;
     }
     if (humanChoise === computerChoise) {
-        result.innerHTML = "Tie 🟰";
-        score.innerHTML = `${humanScore} : ${computerScore}`;
+        result.textContent = "Tie 🟰";
+        score.textContent = `${humanScore} : ${computerScore}`;
     }
     if (humanScore === 5) {
-        result.innerHTML = "You Win";
-        score.innerHTML = `${humanScore} : ${computerScore}`;
-        stopGame();
+        result.textContent = "Game Over: User Win";
+        score.textContent = `${humanScore} : ${computerScore}`;
+        rock.style.display = "none";
+        paper.style.display = "none";
+        scissors.style.display = "none";
+        reset.style.display = "none";
     } else if (computerScore === 5) {
-        result.innerHTML = "You Win Computer";
-        score.innerHTML = `${computerScore} : ${humanScore}`;
-        stopGame();
+        result.textContent = "Game Over: Computer Win";
+        score.textContent = `${computerScore} : ${humanScore}`;
+        rock.style.display = "none";
+        paper.style.display = "none";
+        scissors.style.display = "none";
+        reset.style.display = "none";
     }
 }
 
@@ -83,4 +104,3 @@ function playGame() {
 }
 
 playGame();
-
